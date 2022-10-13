fe1aed4248d91f420f10e823d5b31e642ab257f9
Jay Froment-Rudder <jfromentrudder@gmail.com>
2022-10-13 03:21:04 -0400

    General small updates to main page

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 import express from 'express';
 const app = express();
 import path from 'path';
-import methodOverride from 'method-override';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
+app.use(express.static('public'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-	res.render('/index');
+	res.render('index');
 });
 
 app.listen(3000, () => {
