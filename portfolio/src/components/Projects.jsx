0b18be7679330f707a20b8b5ab8ad5606e7f0a7f
Jay Froment-Rudder <jfromentrudder@gmail.com>
2022-10-20 23:32:32 -0400

    Initial basic setup

---
+++ portfolio/src/components/Projects.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Projects = () => {
+	return <div>Projs</div>;
+};
+
+export default Projects;
---
+++ portfolio/src/components/SideNav.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SideNav = () => {
+	return <div>hi</div>;
+};
+
+export default SideNav;
