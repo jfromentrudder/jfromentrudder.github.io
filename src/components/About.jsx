983e0897a10b8af68dc86a5b580628c7f6b6fddd
Jay Froment-Rudder <jfromentrudder@gmail.com>
2022-11-21 18:58:04 -0500

    Add Full Name to about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
             <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
                 <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
                     <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-                        Nice to Meet You! I'm Jay,
+                        Nice to Meet You! I'm Jay Froment-Rudder,
                         <br className="hidden lg:inline-block" /> Software Engineer
                     </h1>
                     <p className="mb-8 text-lg leading-relaxed">
